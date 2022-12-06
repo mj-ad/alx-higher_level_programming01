6848bfef9fee22076ebab5efe6d0f0a12764f32c
mj-ad <adekunlemojolajah@gmail.com>
2022-12-06 20:21:20 +0100

    Correcting task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in range(0, len(my_list) - 1):
-        if idx < 0:
+    for i in my_list:
+        if idx < 0 or idx > (len(my_list) - 1):
             return None
-        if idx > (len(my_list) - 1):
+        elif idx == my_list.index(i):
+            return i
+        else:
             return None
-        if idx == i:
-            return my_list[i]
