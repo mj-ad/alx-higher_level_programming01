afe2f0237d5d9ab0ab9e3c34505c26fb5873199a
mj-ad <adekunlemojolajah@gmail.com>
2022-12-12 13:16:46 +0100

    Tuples addition mandatory

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == len(tuple_b):
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
         new_tuple = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
         return new_tuple
     if len(tuple_b) == 0:
         return tuple_a
     if len(tuple_a) == 0:
         return tuple_b
-    if len(tuple_a) > len(tuple_b):
+    if len(tuple_a) >= 2 and len(tuple_b) < 2:
         new_tuple = ((tuple_a[0] + tuple_b[0]), (tuple_a[1]))
         return new_tuple
-    if len(tuple_a) < len(tuple_b):
+    if len(tuple_a) < 2 and len(tuple_b) >= 2:
         new_tuple = ((tuple_a[0] + tuple_b[0]), (tuple_b[1]))
         return new_tuple
-    if len(tuple_a) < len(tuple_b):
-        new_tuple = ((tuple_a[0] + tuple_b[0]), (tuple_b[1]))
-        return new_tuple
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return (0, 0)
