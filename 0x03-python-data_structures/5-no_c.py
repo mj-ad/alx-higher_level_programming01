177b1bc6e2a12ac7b09994ba0eaf1f2e23136a3e
mj-ad <adekunlemojolajah@gmail.com>
2022-12-12 10:49:00 +0100

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    d = ''
+    for i in range(0, len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            continue
+        if my_string[i] != 'c' or my_string[i] != 'C':
+            print(my_string[i], end='')
+    return d
