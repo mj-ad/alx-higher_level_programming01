b9816a4ae49e5fa60125a293d953a4650d304be7
mj-ad <adekunlemojolajah@gmail.com>
2022-12-20 12:29:48 +0100

    Changes

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     while True:
         try:
-            int(value) == value
             print('{:d}'.format(value))
             return True
-        except ValueError:
+        except BaseException:
             return False
