9b856591a4de9eaef8d1c6690b7c4a5dfa03296c
mj-ad <adekunlemojolajah@gmail.com>
2022-12-26 15:36:25 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" A class Square that defines a square """
+
+
+class Square:
+    """ Attributes:
+             size (int): Exception error
+    """
+    def __init__(self, size):
+        self.__size = size
