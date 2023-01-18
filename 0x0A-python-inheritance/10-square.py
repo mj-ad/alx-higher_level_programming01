dc51df38b2ae1b317476a4d7f83f7c11ac271d1d
mj-ad <adekunlemojolajah@gmail.com>
2023-01-18 13:49:27 +0100

    Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,9 +7,13 @@ class Square:
     """ inherits from class rectangle """
 
     def __init__(self, size):
+        """ initialization """
+        
         self.integer_validator('size', size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
+        """ area of a square """
+        
         return self.__ size ** 2
