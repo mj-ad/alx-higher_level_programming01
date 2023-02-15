56b55e091c4c109232bc583747ac6748c836dad8
“Mojolajah <adekunlemojolajah@gmail.com>
2023-02-15 13:19:01 +0100

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,4 @@
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE hbtn_0c_0
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
