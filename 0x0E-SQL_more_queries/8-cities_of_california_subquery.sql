e750adce746e1d3bec6bd1517ad4807ad5426d34
“Mojolajah <adekunlemojolajah@gmail.com>
2023-02-17 10:35:20 +0100

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE states.name = 'CALIFORNIA'
+WHERE states.id = cities.state_id
+AND state.name = 'California'
