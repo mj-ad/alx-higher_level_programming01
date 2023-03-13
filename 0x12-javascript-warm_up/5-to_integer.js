aae5bf25cf861002df92a2a713816c9825a2365c
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-13 17:53:29 +0100

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+const arg = process.argv[2];
+if (isNaN(arg)) {
+  console.log('Not a Number');
+} else {
+  const num = parseInt(arg);
+  console.log('My number: ' + num);
+}
