80820fa898bb4c6a4301b0ba60f0393d5f982693
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-14 08:58:35 +0100

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = array.length;
+for (let index = 0; index < len; index++) {
+  console.log(array[index]);
+}
