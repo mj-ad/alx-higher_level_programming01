eaa2e2c8239e4bdcc271b043162811c37ca18010
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-14 11:23:11 +0100

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+const len = process.argv.length;
+if (len < 4) {
+  console.log(0);
+} else {
+  let array = process.argv.slice(2, len - 1);
+  array.sort(function(a, b) {return a - b});
+  console.log(array[(array.length - 2)]);
+}
