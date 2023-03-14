3548d526a59e84cba8adbc7033c81be23e260fc0
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-14 08:48:49 +0100

    Correction

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const arg = process.argv[2];
 if (isNaN(arg)) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else {
   const num = parseInt(arg);
   console.log('My number: ' + num);
