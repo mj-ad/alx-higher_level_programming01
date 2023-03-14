614f84c72402cc03adc2c46124d3a1945901bdae
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-14 11:30:51 +0100

    Correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const len = process.argv.length;
 if (len < 4) {
   console.log(0);
 } else {
-  const array = process.argv.slice(2, len - 1);
+  const array = process.argv.slice(2, len);
   array.sort(function (a, b) { return a - b; });
   console.log(array[(array.length - 2)]);
 }
