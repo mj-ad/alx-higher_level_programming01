7c5902d83ae3621bbe080d1f3b36db2df5ab156f
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-16 19:57:39 +0100

    Corrections

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const mul = list.map((x) => (x * list.indexOf(x)));
+const lis = [];
+const len = list.length
+for (let i = -1; i < len; i++) {
+    lis.push(i);
+}
+const mul = list.map((x, i) =>  x * i);
 console.log(mul);
