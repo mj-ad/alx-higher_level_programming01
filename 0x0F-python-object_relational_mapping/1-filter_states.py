c247c6a2e16adf098a4eba09a636a970ba6ae7b2
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-20 12:32:29 +0100

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Printing states with upper case N
+"""
+import MySQLdb
+from sys import argv
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE LEFT(name,1)='N'")
+    [print(name) for name in cur.fetchall()]
