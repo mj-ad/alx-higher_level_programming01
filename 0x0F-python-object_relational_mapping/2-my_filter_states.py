d59f66a347d5ac3a99bcb0f24bbe932f32c01eef
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-24 16:02:28 +0100

    Perfecting task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,6 +9,5 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                 WHERE name LIKE BINARY '{}'\
-                 ORDER BY `state.id` ASC".format(argv[4]))
+                 WHERE name LIKE BINARY '{}'".format(argv[4]))
     [print(name) for name in cur.fetchall()]
