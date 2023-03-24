3aa384c6b7c6be3d99c04b587881c237200e3fed
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-24 16:27:53 +0100

    Perfecting task 2

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""safely filters the table against sql injections"""
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                 WHERE name = '{}'".format(argv[4]))
+    [print(name) for name in cur.fetchall()]
