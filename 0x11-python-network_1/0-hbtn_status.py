f2a1e7731c0afd3d7500167e0b7ad029a480056f
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-31 15:30:09 +0100

    Perfecting task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 """
 Python script that fetches https://alx-intranet.hbtn.io/status
 """
-from urllib.request import Request,urlopen
+from urllib.request import Request, urlopen
 if __name__ == '__main__':
     req = Request('https://alx-intranet.hbtn.io/status')
     with urlopen(req) as response:
         body = response.read()
         print('Body response:$')
-        print('    - type: ', type(body))
-        print('    - content: ', body)
-        print('    - utf8 content: ', body.decode('utf8'))
+        print('\t- type: ', type(body))
+        print('\t- content: ', body)
+        print('\t- utf8 content: ', body.decode('utf8'))
