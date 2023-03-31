3410691899ce87eebc19ddf5228870ed2644810f
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-31 15:52:13 +0100

    Perfecting task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     with urlopen(req) as response:
         body = response.read()
         print('Body response:$')
-        print('\t- type: ', type(body))
-        print('\t- content: ', body)
-        print('\t- utf8 content: ', body.decode('utf8'))
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf8')))
