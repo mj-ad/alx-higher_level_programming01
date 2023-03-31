0c9e43229b341db634d7a6031ca399bef653d337
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-03-31 12:07:24 +0100

    Perfecting Task8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument and displays the response body
-curl -sX POST $1 -d $2
+curl -sX POST $1 -H "Content-type: application/json" -d @$2
